c361d6d90b3fd4d09e4b72ba081649bebed78152
Mohammedalduraidi <talomari592@gmail.com git config --global user.email talomari592@gmail.com>
2018-07-18 16:17:51 +0300

    install python and mysql

--- part1-Basic Python Server/server.py
+++ part1-Basic Python Server/server.py
@@ -1,15 +1,15 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 #Start by importing myHandler Class from requestHandler.py file
-
+from requestHandler import myHandler
 #Every server needs to listen on a port with a unique number. The
 #standard port for HTTP servers is port 8000,
-PORT_NUMBER = 8080
+Port = 8080
 
 try:
 	#This statement creates a web server and define the handler to manage the
 	#incoming request
-	server = HTTPServer(('', PORT_NUMBER), 'Fill_Me_In')
-	print 'Started httpserver on port ' , PORT_NUMBER
+	server = HTTPServer(('', Port), myHandler)
+	print 'Started httpserver on port ' , Port
 	
 	#Wait forever for incoming htto requests
 	server.serve_forever()
